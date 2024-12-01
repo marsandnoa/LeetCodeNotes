0f3fad9af4422656337872d13eab7320b7f823e7
marsandnoa <marsandnoa@gmail.com>
2024-12-01 17:59:26 -0500

    new leetcode repo

--- obj/Debug/net8.0/leetcode.AssemblyInfo.cs
+++ obj/Debug/net8.0/leetcode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f2293fc20e35afe5d19e3c60deed67ccb626b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9dce0298bff41ca43222d452d09f035f3517f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("leetcode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
